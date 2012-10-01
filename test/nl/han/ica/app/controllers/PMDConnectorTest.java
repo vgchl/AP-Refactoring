e728c858c16533bb073b1354fb4a3171294cd78b
Corne <cornevbeek123@live.nl>
2012-10-01 12:22:32 +0200

    added test folder and classes for current classes

---
+++ test/nl/han/ica/app/controllers/PMDConnectorTest.java
@@ -0,0 +1,28 @@
+package nl.han.ica.app.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Corne
+ * Date: 1-10-12
+ * Time: 12:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class PMDConnectorTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testRunPMD() throws Exception {
+
+    }
+
+    @Test
+    public void testProcessResults() throws Exception {
+
+    }
+}
