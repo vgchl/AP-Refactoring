0999af0c7a7dae5c32b62c4dd47a6e03412ab147
Corne <cornevbeek123@live.nl>
2012-10-03 12:02:58 +0200

    updated StrategyTest.java

--- src/main/java/nl/han/ica/core/strategies/Strategy.java
+++ src/main/java/nl/han/ica/core/strategies/Strategy.java
@@ -12,11 +12,15 @@ import org.eclipse.jdt.core.dom.AST;
  */
 public abstract class Strategy  {
 
+    private AST ast = null;
+
     public abstract void rewriteAST(AST ast);
 
     public void buildAST(RuleViolation ruleViolation) {
         //To change body of implemented methods use File | Settings | File Templates.
     }
 
-
+    public AST getAst() {
+        return ast;
+    }
 }
