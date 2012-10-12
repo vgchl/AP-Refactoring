9779622388c8f18a3150be06e4457ae4176fba09
Teun van Vegchel <teun@veggle.nl>
2012-10-12 15:47:25 +0200

    Editors are now read-only. Fixes #27

--- src/main/resources/editor/editor.js
+++ src/main/resources/editor/editor.js
@@ -4,6 +4,7 @@ editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
     onCursorActivity: function() {
         editor.setLineClass(hlLine, null, null);
         hlLine = editor.setLineClass(editor.getCursor().line, null, "activeline");
-    }
+    },
+    readOnly: true
 });
 var hlLine = editor.setLineClass(0, "activeline");
