bb9d1c677b0e75b2e8c1bc8dd503fb91b4b3ea9f
Corne van Beek <cornevbeek123@live.nl>
2012-10-24 13:57:15 +0200

    updated after view to document instead of compilationunit

--- src/main/java/nl/han/ica/core/Job.java
+++ src/main/java/nl/han/ica/core/Job.java
@@ -112,7 +112,7 @@ public class Job {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        solution.setAfter(strategySolver.getCompilationUnit().toString());
+        solution.setAfter(strategySolver.getDocument().get());
         logger.info("Done solving issue.");
         return solution;
     }
