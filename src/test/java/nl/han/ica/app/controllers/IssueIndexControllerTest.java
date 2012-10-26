bcdb0d49df5d84b543dbec0f4a252df0d4431927
Wouter Konecny <wouterkonecny@gmail.com>
2012-10-26 11:07:51 +0200

    Added test classes so ALL classes have test classes.

---
+++ src/test/java/nl/han/ica/app/controllers/IssueIndexControllerTest.java
@@ -0,0 +1,25 @@
+package nl.han.ica.app.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author: Wouter Konecny
+ * @created: 26-10-12
+ */
+public class IssueIndexControllerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testInitialize() throws Exception {
+
+    }
+
+    @Test
+    public void testGetView() throws Exception {
+
+    }
+}
