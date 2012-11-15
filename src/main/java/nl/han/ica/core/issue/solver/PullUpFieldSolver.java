8ac1e4d705cb5bdc4783ef5d76972d4ff3b53e4b
Niek Versteege <niekversteege@gmail.com>
2012-11-15 14:27:01 +0100

    Initial PullUpFieldSolver version.

--- src/main/java/nl/han/ica/core/issue/solver/PullUpFieldSolver.java
+++ src/main/java/nl/han/ica/core/issue/solver/PullUpFieldSolver.java
@@ -2,10 +2,11 @@ package nl.han.ica.core.issue.solver;
 
 import nl.han.ica.core.Parameter;
 import nl.han.ica.core.Solution;
-import nl.han.ica.core.SourceFile;
 import nl.han.ica.core.issue.Issue;
 import nl.han.ica.core.issue.IssueSolver;
 import nl.han.ica.core.issue.detector.PullUpFieldDetector;
+import org.apache.log4j.Logger;
+import org.eclipse.jdt.core.dom.ASTNode;
 
 import java.util.List;
 import java.util.Map;
@@ -15,9 +16,7 @@ import java.util.Map;
  */
 public class PullUpFieldSolver extends IssueSolver {
 
-    private static final String FIELD_NAME = "Field name";
-
-    private List<SourceFile> subclasses;
+    private final Logger log = Logger.getLogger(getClass().getName());
 
     @Override
     public boolean canSolve(Issue issue) {
@@ -27,6 +26,12 @@ public class PullUpFieldSolver extends IssueSolver {
     @Override
     protected Solution internalSolve(Issue issue, Map<String, Parameter> parameters) {
 
+        Solution solution = new Solution(issue, this, parameters);
+        List<ASTNode> duplicateFields = issue.getNodes();
+
+        getSuperClass(duplicateFields);
+
+
         // get fields to remove from Issue
         // get containing class from fields
         // get superclass from containing classes
@@ -34,6 +39,20 @@ public class PullUpFieldSolver extends IssueSolver {
         // add a field to the superclass
         // remove fields from subclasses
 
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return solution;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    private ASTNode getSuperClass(List<ASTNode> duplicateFields) {
+
+        ASTNode classWithDuplicateField = duplicateFields.get(0).getParent();
+
+        /* Get parent to get the class. Getparent on class to get superclass.*/
+        // TODO: get superclass type
+        ASTNode superclass = classWithDuplicateField;
+
+        log.debug(superclass.toString());
+
+
+        return null;
     }
 }
