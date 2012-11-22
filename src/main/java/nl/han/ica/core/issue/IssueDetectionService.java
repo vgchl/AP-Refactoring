4fd9fa96d64a859fa214431ce49193970d593a5c
Teun van Vegchel <teun@veggle.nl>
2012-11-22 09:01:59 +0100

    Added documentation to the IssueSolvingService and the IssueDetectionService. Improved clarity by renaming the various solve(Issue) methods to createSolution(Issue).

--- src/main/java/nl/han/ica/core/issue/IssueDetectionService.java
+++ src/main/java/nl/han/ica/core/issue/IssueDetectionService.java
@@ -6,14 +6,27 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Provides issue detection functionality. Uses a set of {@link IssueDetector}s to find {@link Issue}s in a set of
+ * {@link CompilationUnit}s.
+ */
 public class IssueDetectionService {
 
     private Set<IssueDetector> detectors;
 
+    /**
+     * Instantiate a new IssueDetectionService.
+     */
     public IssueDetectionService() {
         this.detectors = new HashSet<>();
     }
 
+    /**
+     * Let all the {@link IssueDetector}s scan the {@link CompilationUnit}s and find the {@link Issue}s.
+     *
+     * @param compilationUnits The compilation units to scan though for issues.
+     * @return The detected issues.
+     */
     public Set<Issue> detectIssues(Set<CompilationUnit> compilationUnits) {
         Set<Issue> issues = new HashSet<>();
         for (IssueDetector detector : detectors) {
@@ -25,14 +38,29 @@ public class IssueDetectionService {
         return issues;
     }
 
+    /**
+     * Returns the list of issue detectors.
+     *
+     * @return The list of issue detectors.
+     */
     public Set<IssueDetector> getDetectors() {
         return Collections.unmodifiableSet(detectors);
     }
 
+    /**
+     * Add an issue detector to this service.
+     *
+     * @param issueDetector The issue detector to add.
+     */
     public void addDetector(IssueDetector issueDetector) {
         detectors.add(issueDetector);
     }
 
+    /**
+     * Remove an issue detector from this service.
+     *
+     * @param issueDetector The issue detector to remove.
+     */
     public void removeDetector(IssueDetector issueDetector) {
         detectors.remove(issueDetector);
     }
