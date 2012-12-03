63bfd0be74e0a3902d94d12baa7281eba4856394
Teun van Vegchel <teun@veggle.nl>
2012-12-03 16:24:20 +0100

    Added a testclass to test the detector algorithm with.

---
+++ src/main/resources/issueclasses/encapsulate_collection/PublicFieldCollection.java
@@ -0,0 +1,15 @@
+package issueclasses.encapsulate_collection;
+
+import java.util.*;
+
+public class PublicFieldCollection {
+
+    public Collection<Object> objectCollection;
+
+    public List<Object> objectList;
+    public ArrayList<Object> objectArrayList;
+
+    public Set<Object> objectSet;
+    public HashSet<Object> objectHashSet;
+
+}
